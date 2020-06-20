27c61e6224922171a2b52f62815f86a99b55243d
Tony Bernal <tony@txobijuan.org>
2020-06-20 18:31:52 -0500

    test

--- src/examples/06_module/01_bank/bank_account.h
+++ src/examples/06_module/01_bank/bank_account.h
@@ -1,1 +1,18 @@
 //bank_account.h
+class BankAccount
+{
+public: 
+ BankAccount() {balance = 500;}
+ BankAccount(int b) : balance{b} {}
+ int get_balance() const
+    {
+     return balance;
+
+     }
+   void deposit(int amount);
+   void withdraw(int amount);
+
+private:
+ int balance{0};
+
+};
--- src/examples/06_module/01_bank/main.cpp
+++ src/examples/06_module/01_bank/main.cpp
@@ -1,6 +1,20 @@
+#include "bank_account.h"
+#include<iostream>
+
+using std::cout;
 
 int main()
 {
+	BankAccount account(1000);
+
+	int balance = account.get_balance();
+
+	cout<<"Balance: "<<balance;
+
+	int deposit;
+	cout<<"Enter the deposit amount: ";
+	cin>>deposit;
+	
 
 	return 0;
 }
