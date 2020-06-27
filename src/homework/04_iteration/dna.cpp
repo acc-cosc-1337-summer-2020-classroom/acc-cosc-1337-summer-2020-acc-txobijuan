68c96bb55260806dd354d2c4b8a5e82ee891082b
Tony Bernal <tony@txobijuan.org>
2020-06-26 23:59:01 -0500

    ArthurBernal HMWK 20200627

--- src/homework/04_iteration/dna.cpp
+++ src/homework/04_iteration/dna.cpp
@@ -7,7 +7,33 @@ Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
 
+double get_gc_content(const std::string str)
 
+{
+    double num_g;
+    double num_c;
+    double dna_length;
+    double gc_content;
+    num_g = 0.0;
+    num_c = 0.0;
+    dna_length = 0.0;
+    gc_content = 0.0;
+
+    for(std::size_t i=0; i < str.size(); ++i)
+       { if(str[i] == 'G')
+           { num_g ++;
+            dna_length ++;}
+        else if(str[i] == 'C')
+            {num_c ++;
+            dna_length ++;}
+        else 
+            {dna_length ++;}
+       }
+    
+    gc_content = (num_g + num_c)/dna_length;
+    return gc_content;
+
+}
 
 
 /*
@@ -15,7 +41,16 @@ Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
 
-
+std::string get_reverse_string(std::string str)
+{
+    std::string reverse;
+    reverse = "";
+    for (std::size_t i = str.size() - 1; i >= 0; --i)
+        {
+           reverse = reverse.push_back(str[i]);
+        }
+    return reverse;
+}
 
 /*
 Write prototype for function get_dna_complement that
--- test/homework_test/04_iteration_test/04_iteration_tests.cpp
+++ test/homework_test/04_iteration_test/04_iteration_tests.cpp
@@ -1,7 +1,20 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "dna.cpp"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Verify GC Content")
+{
+	REQUIRE(get_gc_content("AGCTATAG") == .375);
+	REQUIRE(get_gc_content("CGCTATAG") == .50);
+
+}
+
+TEST_CASE("Verify reverse DNA string")
+{
+	REQUIRE(get_reverse_string("AGCTATAG") == "GATATCGA");
+    REQUIRE(get_reverse_string("CGCTATAG") == "GATATCGC");
+}	
